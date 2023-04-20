55b8d7b51092b635d2c1d70ed60847c37b7bf5d7
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-04-20 13:28:39 +0200

    ajout connexion API

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocationResponse, TemperatureService } from 'src/app/services/temperature.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+cityInfo? : LocationResponse;
+
+
+constructor(private temperatureService : TemperatureService) {};
+
+ngOnInit() {
+  this.temperatureService.getLocation().subscribe((next) => this.cityInfo = next);
+  console.log("test");
+  
+}
+
 
 }
